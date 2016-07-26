5ff30af0a5ab16f8919c0100b526a646421d01cc
Matt Badger <matt@lighthouseuk.net>
2016-07-26 16:27:51 +0100

    Version bump and manifest

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='koalaoauth2',
     packages=['koalaoauth2'],  # this must be the same as the name above
-    version='0.1.1-alpha',
+    version='0.1.2-alpha',
     description='',
     author='Matt Badger',
     author_email='foss@lighthouseuk.net',
